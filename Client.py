10bf3e616bd50058d603f0907ba4534d245e45b7
Alvaro <alvvar055@gmail.com>
2020-11-08 11:35:38 +0100

    Added Client.py & Client.config

---
+++ Client.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+import Ice
+Ice.loadSlice('slice.ice')
+import IceGauntlet
+
+class RoomsToolClient(Ice.Application):
+	
+
+class Client(Ice.Application):
+	
